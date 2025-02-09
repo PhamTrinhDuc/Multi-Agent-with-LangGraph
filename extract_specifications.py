598582be5572e540207ce87b5bafd6211e454920
PhamTrinhDuc <duc78240@gmail.com>
2025-02-09 21:47:50 +0700

    extract user's query to specifications

--- extract_specifications.py
+++ extract_specifications.py
@@ -1,31 +1,51 @@
 import os
 import dotenv
-import pandas as pd 
 from openai import OpenAI
-from utils import parse_string_to_dict
+from groq import Groq
+from typing import Literal
+from utils import parse_string_to_dict, Logger
 from prompt import PROMPT_SYSTEM, FUNC_CALL_TOOLS
 
 dotenv.load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
 
 
-def extract_info(client, 
-                 model: str, 
-                 query_user: str, 
-                 tools_calling: list[dict],
-                 prompt_sys: str=""):
+LOGGER = Logger(name=__file__, log_file="extract_query.log")
 
+
+def extract_info(query_user: str,
+                 tools_calling: list[dict]=FUNC_CALL_TOOLS,
+                 prompt_sys: str=PROMPT_SYSTEM['extract_query'],
+                 type_client: Literal["groq", 'openai']="groq"):
+    
     messages = [
         {'role': 'system', 'content': prompt_sys},
         {"role": "user", "content": query_user}]
 
-    openai_response = client.chat.completions.create(
-            model=model,
-            messages=messages,
-            tools=tools_calling,
-            tool_choice="auto",  # auto is default, but we'll be explicit
-        )
-    arguments = openai_response.choices[0].message.tool_calls[0].function.arguments
+    try:
+        if type_client == 'groq':
+            client = Groq()
+            response = client.chat.completions.create(
+                model="llama-3.3-70b-specdec",
+                messages=messages,
+                tools=tools_calling,
+                tool_choice="auto",  # auto is default, but we'll be explicit
+            )
+
+        else:
+            client = OpenAI()
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=messages,
+                tools=tools_calling,
+                tool_choice="auto",  # auto is default, but we'll be explicit
+            )
+
+    except Exception as e:
+        LOGGER.log.error(f"An error occurred while create client: [{type_client}]. Error: {str(e)}")
+
+    arguments = response.choices[0].message.tool_calls[0].function.arguments
     
     specifications = parse_string_to_dict(arguments)
     return specifications
@@ -33,7 +53,6 @@ def extract_info(client,
 
 def main():
     arguments = extract_info("Tôi muốn mua điều hòa từ 10 đến 12 triệu")
-    print(arguments)
     for key, value in arguments.items():
         print(f"{key}: {value}")
 
