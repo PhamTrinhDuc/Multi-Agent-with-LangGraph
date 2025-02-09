3b5349775897ea318215512f8d474288d3d55116
PhamTrinhDuc <duc78240@gmail.com>
2025-02-09 21:36:47 +0700

    vectorstore retriever

--- vectorstore/chroma.py
+++ vectorstore/chroma.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from uuid import uuid4
 from typing import List, Dict, Any
+from dataclasses import dataclass
 from langchain_core.documents import Document
 from langchain_community.vectorstores import Chroma
 from langchain.retrievers import EnsembleRetriever
@@ -9,17 +10,18 @@ from langchain_community.retrievers import BM25Retriever
 from langchain_core.embeddings import Embeddings
 
 from base import BaseRetriever
-from utils.logger import Logger
+from utils import Logger
+from config import ArgChroma
 
-LOGGER = Logger(name=__file__, log_file="chroma_db_retriever.log")
+LOGGER = Logger(name=__file__, log_file="chroma_retriever.log")
 
-
+@dataclass
 class EnsembleQueryEngine(BaseRetriever):
-    def __init__(self, 
-                 embedder: Embeddings, 
-                 df: pd.DataFrame,
-                 weights_ensemble: List[float],
-                 db_persist_path: str="./data/db/chroma_db"):
+    embedder: Embeddings 
+    df: pd.DataFrame
+    config = ArgChroma()
+
+    def __post_init__(self):
         """
         Args: 
             embedder: embedding model 
@@ -28,15 +30,10 @@ class EnsembleQueryEngine(BaseRetriever):
             db_persist_path: db storage directory
 
         """
-        super().__init__()
-        self.embeder = embedder
-        self.df = df
-        self.weights_ensemble = weights_ensemble
-        self.db_persist_path = db_persist_path
 
         self.client = Chroma(
-            embedding_function=embedder, 
-            persist_directory=db_persist_path
+            embedding_function=self.embedder, 
+            persist_directory=self.config.db_persist_path
         )
         self.documents = self.upsert()
 
@@ -54,29 +51,24 @@ class EnsembleQueryEngine(BaseRetriever):
         
         ids = [str(uuid4()) for _ in range(len(documents))]
         
-        if not os.path.exists(self.db_persist_path):
+        if not os.path.exists(self.config.db_persist_path):
             return self.client.from_documents(
                 ids=ids,
-                collection_name=self._COLLECTION_NAME_,
                 documents=documents,
                 embedding=self.embeder,
-                persist_directory=self.db_persist_path
+                persist_directory=self.config.db_persist_path
             )
         LOGGER.log.info(msg="Upsert data to vector db successfull!")
         return documents
     
 
-    def _create_bm25_retriever(self, top_k: int=3) -> BM25Retriever:
+    def _create_bm25_retriever(self) -> BM25Retriever:
         """Create BM25 retriever"""
         retriever = BM25Retriever.from_documents(self.documents)
-        retriever.k = top_k
+        retriever.k = self.config.top_k
         return retriever
     
-    def _create_mmr_retriever(self, 
-                              top_k: int=3, 
-                              lambda_mult: float=0.25, 
-                              fetch_k: int=15, 
-                              filter_search: Dict[str, Any]=None) -> Chroma: 
+    def _create_mmr_retriever(self, filter_search: Dict[str, Any]=None) -> Chroma: 
         """
         top_k: Amount of documents to return (Default: 3)
         fetch_k: Amount of documents to pass to MMR algorithm (Default: 15)
@@ -89,7 +81,9 @@ class EnsembleQueryEngine(BaseRetriever):
             custom: search_kwargs = {'k': 3, 'lambda_mult': 0.25, 'filter': {"product_name": "Dieu_hoa"}}
         """
         
-        filter_default = {'k': top_k, 'lambda_mult': lambda_mult, 'fetch_k': fetch_k}
+        filter_default = {'k': self.config.top_k, 
+                          'lambda_mult': self.config.lambda_mult, 
+                          'fetch_k': self.config.fetch_k}
         if filter_search is not None:
             filter_default['filter'] = filter_search
 
@@ -98,10 +92,7 @@ class EnsembleQueryEngine(BaseRetriever):
             search_kwargs=filter_default
         )
     
-    def _create_vanilla_retriever(self, 
-                                  top_k: int=3,
-                                  score_threshold: float=0.75,
-                                  filter_search: Dict[str, Any]=None) -> Chroma:
+    def _create_vanilla_retriever(self, filter_search: Dict[str, Any]=None) -> Chroma:
         """
         Create vanilla vector similarity retriever
         top_k: Amount of documents to return (Default: 3)
@@ -112,7 +103,9 @@ class EnsembleQueryEngine(BaseRetriever):
             default: search_kwargs = {'k': 3, 'score_threshold': 0.6}
             custom: search_kwargs = {'k': 3, 'score_threshold': 0.25, 'filter': {"product_name": "Dieu_hoa"}}
         """
-        filter_default = {'k': top_k, 'score_threshold': score_threshold}
+        filter_default = {'k': self.config.top_k, 
+                          'score_threshold': self.config.score_threshold}
+        
         if filter_search is not None:
             filter_default['filter'] = filter_search
 
@@ -121,12 +114,7 @@ class EnsembleQueryEngine(BaseRetriever):
             search_kwargs=filter_default
         )
     
-    def _build_ensemble_retriever(self, 
-                                  filter_search: Dict[str, Any]=None,
-                                  top_k: int=3,
-                                  score_threshold: float=0.75,
-                                  lambda_mult: float=0.25, 
-                                  fetch_k: int=15):
+    def _build_ensemble_retriever(self, filter_search: Dict[str, Any]=None,):
         """
         FOR MMR ALGORITHM: 
             fetch_k: Amount of documents to pass to MMR algorithm (Default: 15)
@@ -139,19 +127,16 @@ class EnsembleQueryEngine(BaseRetriever):
         top_k: Amount of documents to return (Default: 3)
         filter_search: Filter by document metadata
         """
-        bm25_retriever = self._create_bm25_retriever(top_k=top_k)
+        bm25_retriever = self._create_bm25_retriever()
 
         # vanilla_retriever = self._create_vanilla_retriever(top_k=top_k,
         #                                                    score_threshold=score_threshold,
         #                                                    filter_search=filter_search)
-        mmr_retriever = self._create_mmr_retriever(top_k=top_k, 
-                                                   fetch_k=fetch_k,
-                                                   lambda_mult=lambda_mult,
-                                                   filter_search=filter_search)
+        mmr_retriever = self._create_mmr_retriever(filter_search=filter_search)
 
         ensemble_retriever=  EnsembleRetriever(
             retrievers=[ bm25_retriever, mmr_retriever],
-            weights=self.weights_ensemble
+            weights=self.config.weights_ensemble
         )
         return ensemble_retriever
     
@@ -165,8 +150,7 @@ class EnsembleQueryEngine(BaseRetriever):
 
     def query(self, 
               query: str, 
-              demands: Dict[str, Any]=None,
-              **params_search):
+              demands: Dict[str, Any]=None):
         """
         Get relevant context for a query about a specific product
         
@@ -181,8 +165,7 @@ class EnsembleQueryEngine(BaseRetriever):
         if demands is not None:
             filter_search = self._create_filter_search(demands=demands)
 
-        retriever = self._build_ensemble_retriever(filter_search=filter_search, 
-                                                  **params_search)
+        retriever = self._build_ensemble_retriever(filter_search=filter_search)
         LOGGER.log.info("Create ensemble retriever successfull!")
 
         contents = retriever.invoke(input=query)
--- vectorstore/qdrant.py
+++ vectorstore/qdrant.py
@@ -1,100 +1,179 @@
 # https://qdrant.tech/articles/vector-search-filtering/
 
 import uuid
-import os
 import json
-from dataclasses import dataclass
-from typing import List, Dict, Any
+import pandas as pd
+from datasets import Dataset
+from tqdm import tqdm
+from typing import Dict, Any
+from dotenv import load_dotenv
+from fastembed import SparseTextEmbedding, TextEmbedding
 from qdrant_client import QdrantClient
 from qdrant_client import models
-from qdrant_client.models import Filter
-from langchain_qdrant import QdrantVectorStore, RetrievalMode, FastEmbedSparse
-from langchain_core.embeddings import Embeddings
-from langchain_core.documents import Document
+from dataclasses import dataclass
+load_dotenv()
+
 from base import BaseRetriever
+from utils import Logger
+from config import ArgQdrant
+
+
+LOGGER = Logger(name=__file__, log_file="qdrant_retriever.log")
+
 
 @dataclass
 class QdrantQueryEngine(BaseRetriever):
     url: str
     api_key: str
-    index_name: str="qdrant_retriever"
+    df: pd.DataFrame
     timeout: int=30
-    def __post_init__ (self, 
-                       embedder: Embeddings, 
-                       documents: Document):
+    batch_size: int=4
+    index_name: str="qdrant_retriever"
+    config = ArgQdrant()
+
+    def __post_init__ (self):
         self.client = QdrantClient(
-            url=self.url, 
+            url=self.url,
             api_key=self.api_key,
             timeout=self.timeout
         )
-        self.embedder = embedder
-        self.documents = documents
-        self.sparse_embeddings = FastEmbedSparse(model_name="Qdrant/bm25")
-        self._create_collection()
-        self.vector_store = self.upsert()
+        self.jina_model = TextEmbedding(model_name="jinaai/jina-embeddings-v2-base-en")
+        self.bm25_model = SparseTextEmbedding(model_name="Qdrant/bm25")
 
-    def _create_collection(self):
-        self.client.recreate_collection(
-            collection_name=self.index_name,
-            optimizers_config=models.OptimizersConfigDiff(indexing_threshold=10000),
-            hnsw_config=models.HnswConfigDiff(
-                m=32,  # Increase the number of edges per node from the default 16 to 32
-                ef_construct=200,  # Increase the number of neighbours from the default 100 to 200
-            ),
-            vectors_config={
-                models.VectorParams(
-                    size=self.embedder.dimensions,
-                    distance=models.Distance.COSINE,
-                ),
-            },
-            sparse_vectors_config={
-                models.SparseVectorParams(
-                    modifier=models.Modifier.IDF,
-                )
-            }
-        )
-    def upsert(self):
-        vector_store = QdrantVectorStore.from_documents(
-            documents=self.documents, 
-            embedding=self.embedder, 
-            sparse_embedding = self.sparse_embeddings,
-            colletion_name=self.index_name,
-            retrieval_mode=RetrievalMode.HYBRID,
-        )
-        return vector_store
-    
-    def query(self, 
-              query: str, 
-              top_k: int, 
-              filter_search: Dict[str, Any]):
+        self.create_collection()
+        self.upsert()
 
-        search_result = self.vector_store.similarity_search_with_score(
-            query=query, 
-            k=top_k, 
-            filter=Filter(
-                must=[
-                    models.FieldCondition(
-                        key="group_product_name", 
-                        match=models.MatchValue(value="đèn năng lượng mặt trời"), 
-                    ),
-                    models.FieldCondition(
-                        key="price",
-                        range=models.Range(
-                            gte=3000000,
-                            lte=5000000
-                        )
-                    )
-                ],
-            ),
-            score_threshold=0.75, 
-        )
-        return search_result
-    
     def _count_data(self):
-        return self.client.count(collection_name=self.index_name)
+        return self.client.count(collection_name=self.index_name).count
     
-    def _delete_collection(self):
+    def _delete_colection(self):
         self.client.delete_collection(collection_name=self.index_name)
+    
+    def create_collection(self):
+        if not self.client.collection_exists(collection_name=self.index_name):
+
+            self.client.create_collection(
+                collection_name=self.index_name,
+                optimizers_config=models.OptimizersConfigDiff(indexing_threshold=10000),
+                hnsw_config=models.HnswConfigDiff(
+                    m=32,  # Increase the number of edges per node from the default 16 to 32
+                    ef_construct=200,  # Increase the number of neighbours from the default 100 to 200
+                ),
+                quantization_config=models.ScalarQuantization(
+                    scalar=models.ScalarQuantizationConfig(
+                        type=models.ScalarType.INT8,
+                        quantile=0.99,
+                        always_ram=True
+                    )
+                ),
+                vectors_config={
+                    "jina-embeddings-v2": models.VectorParams(
+                        size=self.config.dimension_vector,
+                        distance=models.Distance.COSINE,
+                    ),
+                },
+                sparse_vectors_config={
+                    "bm25": models.SparseVectorParams(
+                        modifier=models.Modifier.IDF,
+                    )
+                }
+            )
+            LOGGER.log.info(f"Create index name: {self.index_name} successfull!")
+        else:
+            LOGGER.log.info(f"Collection name: {self.index_name} exists")
+
+
+    def upsert(self, batch_size: int=4):
+        num_data = self._count_data()
+        if num_data > 0:
+            return
+        
+        dataset = Dataset.from_pandas(self.df, preserve_index=False)
+        for batch in tqdm(dataset.iter(batch_size=batch_size), total=len(dataset) // batch_size):
+
+            dense_embedding = list(self.jina_model.embed(documents=batch['group_name']))
+            bm25_embedding = list(self.bm25_model.embed(documents=batch['group_name']))
+
+            self.client.upload_points(
+                collection_name=self.index_name,
+                points=[
+                    models.PointStruct(
+                        id=uuid.uuid4().hex,
+                        
+                        payload={
+                            'product_info_id': batch['product_info_id'][i],
+                            'group_product_name': batch['group_product_name'][i],
+                            'product_name': batch['product_name'][i],
+                            'price': batch['lifecare_price'][i],
+                            'short_description': batch['short_description'][i],
+                            'specifications': batch['specifications'][i],
+                            'file_path': batch['file_path'][i],
+                            'power': batch['power'][i],
+                            'weight': batch['weight'][i],
+                            'volume': batch['volume'][i]
+                        },
+                        vector={
+                            "jina-embeddings-v2": dense_embedding[i].tolist(),
+                            "bm25": bm25_embedding[i].as_object(),
+                        },
+                    ) for i, _ in enumerate(batch["group_name"])
+                ],
+                batch_size=batch_size
+            )
+        LOGGER.log.info(f"Uploading data to Qdrant client successfull !!")
+
+
+    def _create_filter_search(self, demands: Dict[str, Any]):
+        group_product = demands.get("group")
+        filter = models.Filter(
+            must=[
+                models.FieldCondition(
+                    key="group_product_name", 
+                    match=models.MatchValue(value=group_product)
+                ),
+                # models.FieldCondition(
+                #     key="price",
+                #     range=models.Range(
+                #         gte=3000000,
+                #         lte=5000000
+                #     )
+                # )
+            ],
+        )
+        return filter
+
+
+    def query(self, 
+               query: str, 
+               demands: dict[str, any] = None):
+        
+        sparse_embedding = list(self.bm25_model.embed(documents=query))[0]
+        dense_embedding = list(self.jina_model.embed(documents=query))[0]
+
+        filter_search = self._create_filter_search(demands=demands)
+
+        search_result = self.client.query_points(
+            collection_name=self.index_name,
+            prefetch=[
+                models.Prefetch(query=sparse_embedding.as_object(), 
+                                using="bm25", 
+                                limit=self.config.top_k),
+
+                models.Prefetch(query=dense_embedding.tolist(), 
+                                using="jina-embeddings-v2", 
+                                limit=self.config.top_k),
+            ],
+            query=models.FusionQuery(fusion=models.Fusion.RRF), # <--- Combine the scores of the two embeddings
+            query_filter=filter_search,
+            score_threshold=self.config.score_threshold,
+            with_payload=self.config.is_with_payload,
+            with_vectors=self.config.is_with_vector,
+            limit=self.config.top_k,
+        ).points
+
+        response_structured = self.format_output_structure(output_qdrant=search_result)
+        return response_structured
+
 
     def format_output_structure(self, output_qdrant: list):
         outtext = ""
@@ -106,8 +185,3 @@ class QdrantQueryEngine(BaseRetriever):
                 "product_name": {payload['product_name']},
                 "price": {payload['price']} \n"""
         return outtext
-
-if __name__ == "__main__":
-    query = "bán cho tôi Bộ Lưu Trữ Năng Lượng Mặt Trời SUNTEK 12V/25Ah PLUS" 
-    engine = QdrantQueryEngine()
-    engine.testing(query=query)
